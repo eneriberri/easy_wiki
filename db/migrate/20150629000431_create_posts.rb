ba32aa56c55be568861e352935637fe0159ab531
Irene Nguyen <irene.m.nguyen@gmail.com>
2015-06-28 17:33:00 -0700

    posts controller and routes

--- db/migrate/20150629000431_create_posts.rb
+++ db/migrate/20150629000431_create_posts.rb
@@ -1,6 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :body
+      t.string :tags
 
       t.timestamps null: false
     end

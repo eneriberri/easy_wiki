d526d0c954414e34339151936fcaf5657d91ea74
Irene Nguyen <irene.m.nguyen@gmail.com>
2015-06-28 17:36:40 -0700

    posts controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,43 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    render :new
+  end
+
+  def show
+    @posts = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(params[:post])
   
+    if @post.save
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+  
+    if @post.update_attributes(params[:post])
+      render :show
+    else
+      render :json => "ruh roh"
+    end
+  end
+
+  def destroy
+    #TODO
+  end
 end

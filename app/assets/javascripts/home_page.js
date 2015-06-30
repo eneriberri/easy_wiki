6ac08a3230d6e1c0daa49548666e74b2de8001ab
Irene Nguyen <irene.m.nguyen@gmail.com>
2015-06-30 15:24:53 -0700

    add new post and tags to homepage

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -12,9 +12,18 @@ $(document).ready(function() {
   
   $('.search-submit').on('click', submitSearch);
   
-  var editor = new Pen({editor: document.getElementById('write-post'), stay: false});
   
-  $('#write-post').on('click', function(e) {
-    $(e.target).animate({'height': '130px'});
+  //Create editor and animate its functionality
+  var editor = new Pen({editor: document.getElementById('write-post'), stay: false}),
+  $writePost = $('#write-post');
+  
+  $writePost.on('click', function(e) {
+    $writePost.animate({'height': '130px'}, function() {
+      var newTags = '<span class="new-tag-wrapper">'+
+                    '<i class="fa fa-tags"></i><input type="text" class="new-tags" placeholder="Enter tags here">'+
+                    '</span>';
+      $writePost.after(newTags);
+      $('.new-tag-wrapper').fadeIn();
+    });
   });
 });

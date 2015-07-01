e7044c184472264e15aac5bd25388360aea1a39d
Irene Nguyen <irene.m.nguyen@gmail.com>
2015-06-30 20:29:54 -0700

    tag overlay

--- app/assets/javascripts/static_pages/static_pages_index.js
+++ app/assets/javascripts/static_pages/static_pages_index.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   EASY_WIKI.static_pages.index = {
 
     init: function() {
+      
       //Handle search bar functionality
       submitSearch = function() {
         window.location.href = '/tagged?tag=' + $('#search-box').val();
@@ -14,10 +15,10 @@ $(document).ready(function() {
       });
       $('.search-submit').on('click', submitSearch);
 
+      
       //Create editor and animate its functionality
       var editor = new Pen({editor: document.getElementById('write-post'), stay: false}),
       $writePost = $('#write-post');
-
       $writePost.one('click', function(e) {
         $writePost.animate({'height': '180px'}, function() {
           $('.new-tag-wrapper').animate({'opacity': '1'});
@@ -25,6 +26,13 @@ $(document).ready(function() {
           
         });
       });
+      
+      
+      //Top right nav bar
+      $('.fa-bar-link').on('click', function() {
+        $('#tag-overlay').animate({'left': 0});
+      });
+      
     },
   }
 });
